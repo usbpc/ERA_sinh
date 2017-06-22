4427ec22056e981c53cd74f2586e6dd1abb0607f
Kevin Holm <kevin.holm@tum.de>
2017-06-22 13:36:14 +0200

    changed the loop count era_sinh uses

--- implementation/sinh.asm
+++ implementation/sinh.asm
@@ -13,8 +13,12 @@ era_sinh:
 	fninit					;initialize the x87 FPU
 
 	movlps qword [rsp-8], xmm0		;get the double argument x from SSE Register xmm0 into memory to get it into the FPU
-
-	fld qword [precision]			;st0 = precision
+	
+	;this was temporary to test out how many loops I need to run for a specific input x
+	;mov rcx, rdi				;get the loop count in the register that can really use it...
+	
+	;this was used for the old way of ending the loop
+	;fld qword [precision]			;st0 = precision
 	fld qword [rsp-8]			;st0 = x, st1 = precision
 	fxam					;if x=0 C3 = 1, if infinity C2 = C0 = 1
 	fstsw ax				;store x87 FPU SW in ax
@@ -27,6 +31,16 @@ era_sinh:
 	fabs					;st0 = |x|
 
 	fdiv st1, st0				;st1 = x/|x|, eigther 1 or -1
+	
+	fld st0					;I can only get a 64 Bit Integer out of the FPU with popping, so dublicating x here
+	fistp qword [rsp-8]			;get x as integer into memory
+	fwait					;wait that that is done
+	mov rcx, qword [rsp-8]			;then get that integer into rxc
+	shr rcx, 3				;divide it by 8
+	mov rax, rcx				;save that into rax
+	shl rcx, 2				;now multiply rcx with 4
+        add rcx, rax				;add rax to rcx
+	add rcx, 45				;add 45 to rcx, so now rcx = 45+5/8x
 
 	fld1					;load conatant 1
 						;st0 = 1, st1 = x, st2 = sign, st3 = precision	
@@ -39,7 +53,7 @@ era_sinh:
 
 	fld st3					;previous = x
 						;st0 = previous, st1 = i, st2 = result, st3= 1, st4 = x, st5 = sign, st6 = precision
-	fdivr st6, st0				;precision = x/precision
+	;fdivr st6, st0				;precision = x/precision
 .loop:			
 	fincstp					;st7= previous, st0 = i, st1 = result, st2 = 1, st3 = x, st4 = sign, st5 = precision
 	fadd st0, st2				;i++
@@ -55,8 +69,9 @@ era_sinh:
 
 	fadd st2, st0				;result = result + previous
 
-	fcomi st0, st6				;if previous < precision CF = 1
-	jae .loop				;jump if CF = 0
+	;fcomi st0, st6				;if previous < precision CF = 1
+	;jae .loop				;jump if CF = 0
+	loop .loop
 
 	fincstp
 	fincstp					;st6= previous, st7 = i, st0 = result, st1 = 1, st2 = x, st3 = sign, st4 = precision
--- implementation/test.c
+++ implementation/test.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-extern double era_sinh(double x);
-int main() {
-    double d = -711;
+extern double era_sinh(double x, int loops);
+int main(int argc, char *argv[]) {
+    //double d = atof(argv[1]);
+    double d = -10.0;
+    int loops = 1;
+    double ms, s;
     do {
-        double ms = era_sinh(d);
-        double s = sinh(d);
-        printf("d: %f my_sinh: %9.4e sinh: %9.4e delta: %9.4e\n", d, ms, s, ms/s - 1);
-        d += 1.0;
-    } while (d <= 711.0);
+        d += 10.0;
+        loops = 1;
+        do {
+            ms = era_sinh(d, loops);
+            s = sinh(d);
+            //printf("d: %f my_sinh: %9.4e sinh: %9.4e delta: %9.4e\n", d, ms, s, ms/s - 1);
+            loops++;
+        } while (ms != s && loops < 1000);
+        printf("{%f,%d},", d, loops);
+    } while (d <= 700);
+    printf("It took %d loops to make era_sinh as accurate as the build in one for %f\n", loops, d);
     return 0;
 }
