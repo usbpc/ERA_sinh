a1ca2e0bf8f5913a5bcccc27e614518773d52200
K Holm <kevin.holm@tum.de>
2017-07-08 21:00:36 +0200

    removed unneded code

--- implementation/sinh.asm
+++ implementation/sinh.asm
@@ -2,9 +2,6 @@ BITS 64
 
 GLOBAL era_sinh
 
-;section .rodata
-;precision:	dq 10000.0			;define constant precision
-
 section .text
 
 era_sinh:
@@ -14,11 +11,7 @@ era_sinh:
 
 	movlps qword [rsp-8], xmm0		;get the double argument x from SSE Register xmm0 into memory to get it into the FPU
 	
-	;this was temporary to test out how many loops I need to run for a specific input x
-	;mov rcx, rdi				;get the loop count in the register that can really use it...
-	
-	;this was used for the old way of ending the loop
-	;fld qword [precision]			;st0 = precision
+
 	fld qword [rsp-8]			;st0 = x, st1 = precision
 	fxam					;if x=0 C3 = 1, if infinity C2 = C0 = 1
 	fstsw ax				;store x87 FPU SW in ax
@@ -39,7 +32,7 @@ era_sinh:
 	shr rcx, 3				;divide it by 8
 	mov rax, rcx				;save that into rax
 	shl rcx, 2				;now multiply rcx with 4
-        add rcx, rax				;add rax to rcx
+	add rcx, rax				;add rax to rcx
 	add rcx, 45				;add 45 to rcx, so now rcx = 45+5/8x
 	cmp rcx, 489				;compare rcx with 489
 	jbe .passt				;skip the next line if rcx <= 489
@@ -59,7 +52,6 @@ era_sinh:
 
 	fld st3					;previous = x
 						;st0 = previous, st1 = i, st2 = result, st3= 1, st4 = x, st5 = sign, st6 = precision
-	;fdivr st6, st0				;precision = x/precision
 .loop:			
 	fincstp					;st7= previous, st0 = i, st1 = result, st2 = 1, st3 = x, st4 = sign, st5 = precision
 	fadd st0, st2				;i++
@@ -75,12 +67,8 @@ era_sinh:
 
 	fadd st2, st0				;result = result + previous
 
-	;fcomi st0, st6				;if previous < precision CF = 1
-	;jae .loop				;jump if CF = 0
-	dec ecx
-	jnz .loop
-	;apperently the loop instruction is slow on modern cpus...
-	;loop .loop
+	dec ecx					;decrement ecx
+	jnz .loop				;jump to .loop if ecx != 0
 
 	fincstp
 	fincstp					;st6= previous, st7 = i, st0 = result, st1 = 1, st2 = x, st3 = sign, st4 = precision
